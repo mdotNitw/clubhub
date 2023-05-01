48c69012c1b1d50950cebfa4ecffdfcb9c7d1cca
mdotNitw <muqeetmohd@gmail.com>
2023-05-02 02:07:33 +0530

    nocomment

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,8 +1,9 @@
 import "./post.css";
 import { Link } from "react-router-dom";
 
+require('dotenv').config();
 export default function Post({ post }) {
-  const PF = "http://localhost:5000/images/";
+  const PF = `${API}/images/`;
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
